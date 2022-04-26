3fa984a3d92fa33a441f24c53d2c05f8dcebeee1
WenyuanChen1326 <74755505+WenyuanChen1326@users.noreply.github.com>
2022-04-26 16:01:27 -0700

    Add files via upload
    combined all the current work

--- DS3_Global_Health.py
+++ DS3_Global_Health.py
@@ -9,11 +9,15 @@ LifeExp_vs_liver_death = pd.read_csv("LifeExp_vs_liver_death.csv")
 Gdp_vs_femaleH = pd.read_csv("GDP_vs_femaleinthehouse.csv")
 LiteracyRate_vs_year = pd.read_csv("literacy_v_year.csv")
 SatRate_vs_year = pd.read_csv("satisfaction_v_year.csv")
+fm_year_avg = pd.read_csv("female_male_year copy.csv")
+df_male_country = pd.read_csv("male_country copy.csv")
 
-# set up sidebar
+merged_final_pop = pd.read_csv("bmi_population_income.csv")
+merged_year_avg = pd.read_csv("bmi_year_avg.csv")
+# # set up sidebar
 a = st.sidebar
 a.title("Indicators to Explore")
-choice = a.radio("Navigation", ["LifeExpectancy", "Liver Cancer Death", "Lung Cancer Death", "Literacy", "Satisfaction", "%female in The House"])
+choice = a.radio("Navigation", ["LifeExpectancy", "Liver Cancer Death", "Lung Cancer Death", "BMI and Income", "Literacy", "Satisfaction", "%female in The House"])
 # Setting up titles
 st.write("""
 # Global Health Trend of Several Indicators between Male & Female across Continents
@@ -22,7 +26,7 @@ if choice == "LifeExpectancy":
     example = LifeExp_vs_year.groupby(['Year','Indicator Name'])[['LifeExpectancy','Pop']].mean().reset_index()
 
     st.write("""
-    ### Example: Overall Average Life Expectancy VS Year between Male and Female 
+    ### Example: Overall Average Life Expectancy VS Year between Male and Female
     """)
     # the same way as you would draw in jupyter notebook
     LifeExp_AVG_fig = px.scatter(example,x="Year", y="LifeExpectancy",
@@ -63,9 +67,21 @@ if choice == "LifeExpectancy":
             else:
                 df = df[df['Region'] == j]
             break
-    LifeExp_Year_figure = px.scatter(df, x="Year", y="LifeExpectancy",
-                                         size="Pop", color="Indicator Name", symbol="Country Name",
+    if i ==  'All Genders' and j != 'All Regions' :
+        LifeExp_Year_figure = px.scatter(df, x="Year", y="LifeExpectancy",
+                                         size="Pop", color="Indicator Name",
                                          hover_name="country", log_x=True, size_max=60)
+
+    elif j == 'All Regions' and i != 'All Genders' or j == 'All Regions' and i == 'All Genders':
+        LifeExp_Year_figure = px.scatter(df, x="Year", y="LifeExpectancy",
+                                         size="Pop", color="Region",
+                                         hover_name="country", log_x=True, size_max=60)
+    else: 
+        LifeExp_Year_figure = px.scatter(df, x="Year", y="LifeExpectancy",
+                                         size="Pop", color="country",
+                                         hover_name="country", log_x=True, size_max=60)
+
+
     st.plotly_chart(LifeExp_Year_figure)
 
 if choice == "Liver Cancer Death":
@@ -77,19 +93,19 @@ if choice == "Liver Cancer Death":
 
     fig = px.scatter(df, x= "liver_death", y="LifeExpectancy",
                  size="Pop", color= "Indicator Name",symbol = "Indicator Name",
-                     hover_name="Indicator Name", log_x=True, size_max=20)
+                     hover_name="Year", log_x=True, size_max=20)
     fig.update_layout(xaxis_title="liver_death in %")
     st.plotly_chart(fig)
-
-
+    #
+    #
     st.write("""
     ### Explore Life Expectancy VS Liver Cancer Death between Gender for a specific country
     """)
-
+    #
     Country = st.selectbox("Country: ",np.array(LifeExp_vs_liver_death['Country Name'].unique()))
-
+    #
     df = LifeExp_vs_liver_death[LifeExp_vs_liver_death['Country Name'] == Country]
-
+    #
     fig = px.scatter(df,x="liver_death", y="LifeExpectancy",
                  size="Pop", color= "Indicator Name", symbol = "Region",
                      hover_name="Country Name", size_max=40, animation_frame = 'Year')
@@ -97,8 +113,8 @@ if choice == "Liver Cancer Death":
                       xaxis_range=(np.min(df['liver_death']), np.max(df['liver_death']) * 1.2 ),
                      yaxis_range=(0, 100))
     st.plotly_chart(fig)
-
-
+    #
+    #
     st.write("""
     ### Explore Life Expectancy VS Liver Cancer Death Between Genders across regions
     """)
@@ -130,7 +146,7 @@ if choice == "Liver Cancer Death":
     st.plotly_chart(fig)
 if choice == "%female in The House":
     st.write("""
-    ### Example: Overall Percentage of female in The House VS Year  
+    ### Example: Overall Percentage of female in The House VS Year
     """)
     example = Gdp_vs_femaleH.groupby(['Year', 'Indicator Name'])[['GDPs','%female in the house']].mean().reset_index()
     fig = px.scatter(example, x="Year", y="%female in the house",
@@ -182,7 +198,7 @@ if choice == "Literacy":
     example = LiteracyRate_vs_year.groupby(['Year','Indicator Name'])[['Lit','Pop']].mean().reset_index()
 
     st.write("""
-    ### Example: Overall Average Literacy Rate by Subregion VS Year between Male and Female 
+    ### Example: Overall Average Literacy Rate by Subregion VS Year between Male and Female
     """)
     # the same way as you would draw in jupyter notebook
     LitRate_AVG_fig = px.scatter(example,x="Year", y="Lit",
@@ -218,12 +234,12 @@ if choice == "Literacy":
                                          size="Pop", color="Indicator Name", symbol="Subregion",
                                          hover_name="Subregion", log_x=True, size_max=60)
     st.plotly_chart(LitRate_Year_figure)
-    
+
 if choice == "Satisfaction":
     example = SatRate_vs_year.groupby(['Year','Indicator Name'])[['Sat','Pop']].mean().reset_index()
 
     st.write("""
-    ### Example: Overall Average Satisfaction Rate by Subregion VS Year between Male and Female 
+    ### Example: Overall Average Satisfaction Rate by Subregion VS Year between Male and Female
     """)
     # the same way as you would draw in jupyter notebook
     SatRate_AVG_fig = px.scatter(example,x="Year", y="Sat",
@@ -259,6 +275,49 @@ if choice == "Satisfaction":
                                          size="Pop", color="Indicator Name", symbol="Subregion",
                                          hover_name="Subregion", log_x=True, size_max=60)
     st.plotly_chart(SatRate_Year_figure)
+
+if choice == "Lung Cancer Death":
+    example = fm_year_avg.groupby("year").mean().reset_index
+
+    st.write("""
+    ### Example: Overall Average death by year for females and males
+    """)
+    # the same way as you would draw in jupyter notebook
+    male_female_deaths_fig = px.scatter(fm_year_avg, x="year", y=fm_year_avg.columns[1:])
+    male_female_deaths_fig.update_xaxes(tickangle=45)
+    # streamlit function to dispaly plotly drawing. Check the correct function for matplotlib or others.
+    st.plotly_chart(male_female_deaths_fig)
+
+    # countries and death
+    df_male_cancer_fig = px.bar(df_male_country, x="country", y="deaths")
+    st.plotly_chart(df_male_cancer_fig)
+
+    # top 10 countries with deaths --> males
+    top_countries_male = df_male_country[:10]
+    fig_top_male_lc = px.bar(top_countries_male, x="country", y="deaths")
+    st.plotly_chart(fig_top_male_lc)
+
+if choice == "BMI and Income":
+    st.write("""
+    overall trend for BMI per country over years
+    """)
+    # the same way as you would draw in jupyter notebook
+    df = merged_final_pop
+    fig = px.scatter(df, x="income", y="BMI",
+                     color="gender", symbol='country', size='population',
+                     hover_name='country', size_max=20, animation_frame='year')
+    st.plotly_chart(fig)
+
+    st.write("""
+    Average BMI per year for females and males
+    """)
+
+    fig_year_avg = px.scatter(merged_year_avg, x="year", y=merged_year_avg.columns[1:], title=
+    "Average BMI per Year", labels={
+        "value": "BMI",
+    })
+    fig_year_avg.update_xaxes(tickangle=45)
+    st.plotly_chart(fig_year_avg)
 
 
 
